34ee0f58ec818c65bf890709d6022ffdf0d19650
guangongbo <guangongbo@baidu.com>
2014-04-03 12:56:50 +0800

    将编译脚本关联 protobuf 2.3.0 的生成文件

--- windows/gen.bat
+++ windows/gen.bat
@@ -1,4 +1,4 @@
 @echo off
-set protoc_exe=%~dp0protoc.exe
+set protoc_exe=%~dp0..\protobuf_project\protobuf\vsprojects\Release\protoc.exe
 %protoc_exe% -I..\proto --javamicro_out=..\ProtobufJsonDemo\src ..\proto\field_test.proto
 pause
