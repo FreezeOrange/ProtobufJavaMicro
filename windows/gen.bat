22739aac1718fc60c2da4f55f8fe8a44b57b73e4
guangongbo <guangongbo@baidu.com>
2014-04-03 11:20:46 +0800

    提交测试工程

--- windows/gen.bat
+++ windows/gen.bat
@@ -1,4 +1,4 @@
 @echo off
 set protoc_exe=%~dp0protoc.exe
-%protoc_exe% -I..\proto --javamicro_out=.\ ..\proto\field_test.proto
+%protoc_exe% -I..\proto --javamicro_out=..\ProtobufJsonDemo\src ..\proto\field_test.proto
 pause
